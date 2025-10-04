4a94ace27c485fab54d115242d96315252e97f8f
noknight12 <karlonasa12@gmail.com>
2025-10-04 16:11:34 -0700

    circleButton

---
+++ src/app/app/lessons/[unit]/[lesson]/page.jsx
@@ -0,0 +1,17 @@
+import { PrimaryButton } from "@/app/components/Button";
+
+export default () => {
+  return (
+    <main className="p-4">
+      <h1 className="text-4xl">Lesson
+
+         <PrimaryButton>
+            <div className="flex items-center gap-1">
+              Get started 
+            </div>
+          </PrimaryButton>
+      </h1>
+     
+    </main>
+  );
+};
---
+++ src/app/app/lessons/[unit]/page.jsx
@@ -0,0 +1,7 @@
+export default () => {
+  return (
+    <main className="p-4">
+      <h1 className="text-4xl">Unit</h1>
+    </main>
+  );
+};
