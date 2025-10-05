72ca5d717320d0e803b50cc9609936ef751e5f5d
ChakornK <chakornk2007@gmail.com>
2025-10-04 18:37:00 -0700

    feat: add navbar login/user button

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,4 +1,9 @@
+"use client";
+
+import { HugeiconsIcon } from "@hugeicons/react";
 import { PrimaryButton, SecondaryButton } from "./Button";
+import { LogoutSquare01Icon, UserIcon } from "@hugeicons/core-free-icons/index";
+import { useState } from "react";
 
 // TODO: dynamically determine this
 const isLoggedIn = true;
@@ -12,22 +17,60 @@ export const NavBar = () => {
       </div>
 
       {/* User */}
-      <div className="flex items-center mr-1">
-        {isLoggedIn ? (
-          <button className="flex items-center gap-2 hover:bg-neutral-100 px-4 py-2 rounded-lg">
-            <p>Username</p>
-            <div className="border-2 border-gray-200 rounded-full overflow-clip">
-              <svg width="32" height="32" viewBox="0 0 128 128" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <rect width="128" height="128" fill="white" />
-                <path d="M7 128C7 104.804 25.804 86 49 86H79C102.196 86 121 104.804 121 128H7Z" fill="#D9D9D9" />
-                <circle cx="64" cy="48" r="27" fill="#D9D9D9" />
-              </svg>
-            </div>
-          </button>
-        ) : (
-          <PrimaryButton>Log in</PrimaryButton>
-        )}
-      </div>
+      <div className="relative flex items-center mr-1">{isLoggedIn ? <LoggedInUser /> : <PrimaryButton>Log in</PrimaryButton>}</div>
     </nav>
+  );
+};
+
+const LoggedInUser = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  return (
+    <>
+      <button
+        className={`${showMenu ? "bg-neutral-100" : ""} flex items-center gap-2 hover:bg-neutral-100 px-4 py-2 rounded-lg`}
+        onClick={() => setShowMenu(true)}
+      >
+        <p>Username</p>
+        <div className="border-2 border-gray-200 rounded-full overflow-clip">
+          <svg width="32" height="32" viewBox="0 0 128 128" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="128" height="128" fill="white" />
+            <path d="M7 128C7 104.804 25.804 86 49 86H79C102.196 86 121 104.804 121 128H7Z" fill="#D9D9D9" />
+            <circle cx="64" cy="48" r="27" fill="#D9D9D9" />
+          </svg>
+        </div>
+      </button>
+
+      {showMenu && (
+        <>
+          <div
+            className="top-0 right-0 bottom-0 left-0 fixed"
+            onClick={() => {
+              setShowMenu(false);
+            }}
+          ></div>
+          <div className="top-14 right-0 absolute flex *:flex flex-col items-stretch *:items-center *:gap-1 bg-white shadow-lg *:m-1 *:px-2 *:py-1 border-2 border-gray-200 rounded-xl *:rounded-lg min-w-40">
+            <button
+              className="hover:bg-sky-400 border-gray-100 border-b hover:text-white"
+              onClick={() => {
+                setShowMenu(false);
+              }}
+            >
+              <HugeiconsIcon icon={UserIcon} size={16} />
+              Profile
+            </button>
+            <button
+              className="hover:bg-red-500 text- hover:text-white"
+              onClick={() => {
+                setShowMenu(false);
+              }}
+            >
+              <HugeiconsIcon icon={LogoutSquare01Icon} size={16} />
+              Logout
+            </button>
+          </div>
+        </>
+      )}
+    </>
   );
 };
