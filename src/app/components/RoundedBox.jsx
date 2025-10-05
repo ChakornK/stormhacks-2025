4cfce63a3a7f72a025797a505abc4ce2cbf91882
noknight12 <karlonasa12@gmail.com>
2025-10-04 20:51:04 -0700

    Dashboard buttons

--- src/app/components/RoundedBox.jsx
+++ src/app/components/RoundedBox.jsx
@@ -1,12 +1,9 @@
-export const RoundedBox = ({ children, onClick }) => {
+export const RoundedBox = ({ className, children }) => {
   return (
     <div
       
-      className="bg-sky-400 px-6 py-2 inline-block
-     border-sky-500 border-b-6 
-     rounded-2xl 
-     text-white text-lg cursor-pointer"
-      onClick={onClick}
+      className={`bg-sky-400 px-6 py-2 inline-block border-sky-500 border-b-6  rounded-2xl text-white text-lg cursor-pointer ${className}`}
+      
     >
       {children}
     </div>
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -5,28 +5,56 @@ export default function dashboard(){
 
     return(
 
-        <main className="p-4">
-      
-            <RoundedBox>
-             streak
+        <main className="p-4 flex items-center justify-center h-auto">
+            <div className=" max-w-5xl w-full flex items-center justify-center">
+            <div className="grid grid-cols-3 gap-4">
+            <RoundedBox className="h-[20vh]">
+             <p className="text-center">
+                 streak
+             </p>
+             
             </RoundedBox>
       
-            <RoundedBox>
-             ranks
+            <RoundedBox className="h-[20vh]">
+             <p className="text-center">
+                 ranks
+             </p>
             </RoundedBox>
 
-            <RoundedBox>
-             completed lessons
+            <RoundedBox className="h-[20vh]">
+             <p className="text-center">
+                 completed lessons
+             </p>
+             
             </RoundedBox>
 
-            <RoundedBox>
-             completed lessons
+            <RoundedBox className="h-[20vh]">
+             <p className="text-center">
+                 completed units
+             </p>
+             
             </RoundedBox>
 
-             <RoundedBox>
-             practice questions
+            <RoundedBox className="col-span-2 h-30 h-[20vh]">
+            <p className="text-center">
+                 complete
+            </p>
+             
             </RoundedBox>
-     
+
+             <RoundedBox className="col-span-2 h-30 h-[20vh]">
+             <p className="text-center">
+                 Activity
+             </p>
+            </RoundedBox>
+
+            <RoundedBox className="h-[20vh]">
+             <p className="text-center">
+                 smt
+             </p>
+            </RoundedBox>
+     </div>
+     </div>
     </main>
 
     );
