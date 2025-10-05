52d0deeb25a6e2106a87e89f6ed6c3531f33ff9d
ChakornK <chakornk2007@gmail.com>
2025-10-04 22:50:13 -0700

    fix: page sizing

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,9 +1,8 @@
-'use client'
+"use client";
 import { GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
 import { jwtDecode } from "jwt-decode";
 
 export default function Login() {
-
   const handleSuccess = (credentialResponse) => {
     const decoded = jwtDecode(credentialResponse.credential);
     console.log("Decoded token:", decoded);
@@ -14,14 +13,16 @@ export default function Login() {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-b from-sky-50 to-white">
-      <div className="bg-white shadow-xl border border-gray-200 rounded-2xl p-10 w-[90%] max-w-md text-center">
-        <h1 className="text-4xl font-bold bg-gradient-to-b from-sky-400 to-sky-600 bg-clip-text text-transparent mb-6">
+    <div className="flex justify-center items-center bg-gradient-to-b from-sky-50 to-white h-full">
+      <div className="bg-white shadow-xl p-10 border border-gray-200 rounded-2xl w-[90%] max-w-md text-center">
+        <h1 className="bg-clip-text bg-gradient-to-b from-sky-400 to-sky-600 mb-6 font-bold text-transparent text-4xl">
           Welcome!
         </h1>
-        <p className="text-gray-500 mb-8">Sign in to continue to Math Helper</p>
+        <p className="mb-8 text-gray-500">Sign in to continue to Math Helper</p>
 
-        <GoogleOAuthProvider clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}>
+        <GoogleOAuthProvider
+          clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}
+        >
           <div className="flex justify-center">
             <GoogleLogin
               onSuccess={handleSuccess}
